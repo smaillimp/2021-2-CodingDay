fb02de857ec8380a886961a7a178e9e7b0e56035
smaillimp <sp2020.ing@gmail.com>
2021-10-14 15:55:50 +0200

    Afternoon group exercise

---
+++ 08_exercise/scripts.js
@@ -0,0 +1,45 @@
+
+
+var question1 = {
+    q1:"Who invented JavaScript?",
+
+    options = {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+    },
+
+    correctAnswer:"c"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let form = document.querySelectorAll("form");
+
+// for (let i = 0; i < question.length; i++) {
+
+//     let 
+
+// }
+// let body = document.querySelector("body");
+
+// for (let i = 0; i < Question.length; i++) {
+//     let question = Question[i];
+//     let list = document.createElement('li');
+//     list.textContent = question.q1 + question.a1;
+//     body.append(list);
+// }
+
+
